4ab9f4a335d5efdc2a5836f641fcb54685528722
mark.heaver <mark.heave@gmail.com>
2015-05-10 21:43:24 +0100

    shit

---
+++ GNBCWeb/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GNBCWeb.Models;
+
+namespace GNBCWeb.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult EnquirySuccess(ContactViewModel viewModel)
+		{
+			
+			return View(viewModel);
+		}
+
+    }
+}
